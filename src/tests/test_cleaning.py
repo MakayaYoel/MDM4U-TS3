b59489bbe960c524b49d3d2665e9c22fffddc4ef
MakayaYoel <yoelmakaya@gmail.com>
2024-12-02 00:03:34 -0500

    clean data

---
+++ src/tests/test_cleaning.py
@@ -0,0 +1,53 @@
+import csv
+from datetime import datetime
+import os
+
+def test_cleaning():
+    raw_file_path = 'data/raw/1810024501_donneesselectionnees.csv'
+    cleaned_file_path = 'data/cleaned/cleaned_data.csv'
+
+    # Make sure dirs exists
+    os.makedirs(os.path.dirname(cleaned_file_path), exist_ok=True)
+
+    # need utf-8 encoding cause french/latin characters
+    with open(raw_file_path, encoding='utf-8') as csvFile:
+        reader = csv.reader(csvFile, delimiter=';', quotechar="|")
+        data = []
+
+        """
+            Indexes
+            --------
+            0 : Date
+            3 : Product
+            4 : Unit of mesure
+            10 : Price
+        """
+
+        for i, row in enumerate(reader):
+            row = [r.replace('"', '') for r in row]
+
+            if i == 0 or len(row) == 0 or row[4] != "Dollars":
+                continue
+
+            data.append(extract_relevant_data(row))
+
+    save_cleaned_data(data, cleaned_file_path)
+
+        
+
+def extract_relevant_data(row):
+    date = row[0]
+    product = row[3]
+    price = float(row[10])
+    
+    return {"date": date, "product": product, "price": price}
+
+def save_cleaned_data(data, file_path):
+    with open(file_path, 'w', newline='', encoding='utf-8') as csvFile:
+        headers = ['date', 'product', 'price']
+        writer = csv.DictWriter(csvFile, fieldnames=headers)
+        writer.writeheader()
+        writer.writerows(data)
+
+if __name__ == "__main__":
+    test_cleaning()
