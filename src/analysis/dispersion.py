8a988a75139df615d832de593c52a51381157a3a
MakayaYoel <yoelmakaya@gmail.com>
2024-12-02 20:14:39 -0500

    add percentile and interpercentile_range calculation

--- src/analysis/dispersion.py
+++ src/analysis/dispersion.py
@@ -1,6 +1,6 @@
 import sys
 from collections import Counter
-from central_tendency import mean
+from central_tendency import mean, median
 import math
 import statistics
 
@@ -33,9 +33,26 @@ def standard_deviation(data, type=TYPE_SAMPLE):
 def z_score(data, x) :
     return (x - mean(data)) / standard_deviation()
 
+# Credit: https://www.calculatorsoup.com/calculators/statistics/percentile-calculator.php
+def percentile(data, x):
+    sorted_data = sorted(data)
+    r = (x / 100) * (len(sorted_data) - 1) + 1
+
+    if r.is_integer():
+        return sorted_data[int(r)]
+    else:
+        integer_part = int(r)
+        fractional_part = r - integer_part
+        return sorted_data[integer_part - 1] + fractional_part * (sorted_data[integer_part] - sorted_data[integer_part - 1])
+
+def interpercentile_range(data):
+    return percentile(data, 75) - percentile(data, 25)
+
 if __name__ == "__main__":
     prices = [row["price"] for row in data]
+    
     print("-- DISPERSION PAR RAPPORT AUX PRIX -- ")
     print(f"ÉTENDUE: {stats_range(prices)}")
     print(f"VARIANCE: {variance(prices)}")
     print(f"ÉCART-TYPE: {standard_deviation(prices)}")
+    print(f"INTERVALLE INTERQUARTILE: {interpercentile_range(prices)}")
