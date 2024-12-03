3b78b44bfb40ce11d6499b41237277b46c2aad48
MakayaYoel <yoelmakaya@gmail.com>
2024-12-02 21:21:05 -0500

    this is basically a statistics library.

--- src/analysis/central_tendency.py
+++ src/analysis/central_tendency.py
@@ -1,22 +1,22 @@
 from collections import Counter
 
 # Calculates the mode of the given dataset
-def mode(dat):
-    count = Counter(dat)
+def mode(data):
+    count = Counter(data)
     return count.most_common()[0][0]
 
 # Calculates the average of the given dataset to the nearest hundreth
-def mean(dat):
-    return round(sum(dat) / len(dat), 2)
+def mean(data):
+    return round(sum(data) / len(data), 2)
 
 # Calculates the median of the given dataset to the nearest hundreth
-def median(dat):
-    dat = sorted(dat)
-    length = len(dat)
+def median(data):
+    data = sorted(data)
+    length = len(data)
     middle_index = length // 2
 
     # List has an even length, calculate middle of two indexes
     if length % 2 == 0:
-        return round((dat[middle_index - 1] + dat[middle_index]) / 2, 2)
+        return round((data[middle_index - 1] + data[middle_index]) / 2, 2)
     else:
-        return round(dat[middle_index] / 2, 2)
+        return round(data[middle_index] / 2, 2)
